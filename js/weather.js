331a32c08604ff5dd313bcd9154b410ea1cfc3f4
JianSong <a2792839a@gmail.com>
2021-02-20 13:56:19 +0800

    台灣縣市各區天氣完成

---
+++ js/weather.js
@@ -0,0 +1,203 @@
+const app = new Vue({
+  el: ".weather",
+  data: {
+    weatherData: [],//獲得的所有資料
+    currentLNum: 0,//當前地區的陣列索引
+    now: {
+      currentC: "", //當前城市
+      currentL: "", //當前地區
+      regionsList: [],//所有當前城市地區
+      whichDay: 0,//哪一天(0=今天)
+      info: {//當前天氣資訊
+        t: 0,//氣溫
+        wx: "",//天氣現象
+        at: 0,//體感溫度
+        uvi: "",//紫外線
+        rh: 0,//濕度
+        pop12h: 0,//降雨機率
+        wd: "",//風向
+        weatherP: "",//天氣
+        weatherIMG: "",//天氣圖
+      },
+    },
+    CorF: true,//度C或是度F(度C為true)
+    searchBox: "",//搜索框
+    isFocus: false
+  },
+  created() {
+    this.getURL();
+  },
+  methods: {
+    //和中央氣象局連接資料
+    getURL: function (c = "台中市") {
+      const authorization = "CWB-422B0FA3-E374-492D-B54A-4D8942BE2B7E";
+      const url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/";
+
+      c = (c[0] == "台") ? "臺" + c.slice(1) : c;
+      this.now.currentC = c;
+
+      let xhr = new XMLHttpRequest();
+      xhr.open("get", "js/location.json");
+      xhr.responseType = "json";
+      xhr.send();
+      xhr.onload = () => {
+        let response = xhr.response;
+
+        $.each(response.location, (key, obj) => {
+          if (obj.name == c) {
+            let dataId = response.location[key].dataId;
+            let newURL = url + dataId + "?Authorization=" + authorization + "&format=JSON";
+
+            new GetWeather(newURL, this.init);
+          }
+        });
+      }
+    },
+    //初始化
+    init: function (data) {
+      this.now.regionsList = [];
+      this.weatherData = data;
+
+      this.writeMsg();
+
+      $.each(data, (index, obj) => {
+        this.now.regionsList.push(obj.location);
+      });
+      console.log(this.now.regionsList)
+    },
+    //設置天氣資訊
+    writeMsg(now = this.now.whichDay) {
+      this.now.currentL = this.weatherData[this.currentLNum].location;
+
+      this.wxLite(this.weatherData[this.currentLNum].wxV[now]);
+
+      this.now.info.t = this.weatherData[this.currentLNum].t[now];
+      this.now.info.wx = this.weatherData[this.currentLNum].wx[now];
+      this.now.info.at = this.weatherData[this.currentLNum].at[now];
+      this.now.info.uvi = this.weatherData[this.currentLNum].uvi[now];
+      this.now.info.rh = this.weatherData[this.currentLNum].rh[now];
+      this.now.info.pop12h = this.weatherData[this.currentLNum].pop12h[now];
+      this.now.info.wd = this.weatherData[this.currentLNum].wd[now];
+      //console.log(this.now.info);
+    },
+    //判斷晴、陰、雨、雪天
+    wxLite(whichData) {
+      switch (parseInt(whichData)) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 24:
+        case 25:
+        case 26:
+        case 27:
+        case 28:
+          this.now.info.weatherP = "晴";
+          this.now.info.weatherIMG = "images/sunIMG.svg";
+          break;
+
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+          this.now.info.weatherP = "陰";
+          this.now.info.weatherIMG = "images/cloudIMG.svg";
+          break;
+
+        case 42:
+          this.now.info.weatherP = "雪";
+          this.now.info.weatherIMG = "images/snowIMG.svg";
+          break;
+
+        default:
+          this.now.info.weatherP = "雨";
+          this.now.info.weatherIMG = "images/rainIMG.svg";
+          break;
+      }
+    },
+    //溫度換算
+    convertT(index) {
+      let newValue;
+
+      if (this.CorF && index == 1) {
+        newValue = Math.round((this.now.info.t * 9) / 5 + 32);
+        this.CorF = false;
+      } else if (!this.CorF && index == 0) {
+        newValue = Math.round(((this.now.info.t * 1 - 32) * 5) / 9);
+        this.CorF = true;
+      } else {
+        return;
+      }
+
+      this.now.info.t = newValue;
+    },
+    //搜尋地區天氣
+    searchL() {
+      if (this.searchBox == "") {
+        return;
+      }
+
+      let k;
+
+      $.each(this.weatherData, (key, obj) => {
+        if (obj.location == this.searchBox) {
+          k = key;
+        }
+      });
+
+      if (k != undefined) {
+        this.currentLNum = k;
+        this.writeMsg();
+      } else {
+        alert("請輸入此縣市有的地區");
+      }
+
+      this.searchBox = "";
+    },
+    //直接點選地區
+    clickLocal(e, local) {
+      e.stopPropagation();
+      this.searchBox = local;
+      this.isFocus = false;
+    },
+    //眼睛跟隨
+    eyeMove(e) {
+      const eyes = $(".eyes")[0];
+      let eyeLX, eyeRX, eyeLRY;//左眼x、右眼x、左右眼y
+      let x = e.clientX;//鼠標位置x
+      let y = e.clientY;//鼠標位置y
+
+      //算鼠標到天氣圖的距離
+      let directionLX = (x - (eyes.offsetLeft + eyes.offsetWidth / 2)) * 5;
+      let directionLY = (y - (eyes.offsetTop + eyes.offsetHeight / 2)) * 5;
+
+      //算從天氣中心到螢幕右邊的距離
+      let rightWidth = (window.innerWidth - (eyes.offsetLeft + eyes.offsetWidth / 2));
+
+      if (directionLX < 0) {
+        eyeLX = directionLX / (eyes.offsetLeft + eyes.offsetWidth / 2);
+        eyeRX = (directionLX * 2) / (eyes.offsetLeft + eyes.offsetWidth / 2);
+      } else {
+        eyeLX =
+          (directionLX * 2) / rightWidth;
+        eyeRX =
+          directionLX / rightWidth;
+      }
+      if (directionLY < 0) {
+        eyeLRY = directionLY / (eyes.offsetTop + eyes.offsetHeight / 2);
+      } else {
+        eyeLRY =
+          directionLY /
+          (window.innerHeight - (eyes.offsetTop + eyes.offsetHeight / 2));
+      }
+
+      eyes.children[0].style.left = 40 + eyeLX + "%";
+      eyes.children[0].style.top = 45 + eyeLRY + "%";
+
+      eyes.children[1].style.left = 55 + eyeRX + "%";
+      eyes.children[1].style.top = 45 + eyeLRY + "%";
+    }
+  }
+});
---
+++ js/weatherInfo.js
@@ -0,0 +1,115 @@
+class GetWeather {
+  constructor(requestURL, getData) {
+    this.getData = getData;
+    this.dataOK = []; //全部縣市個別天氣資訊
+    this.request = new XMLHttpRequest(); //創建新的伺服器連結
+
+    this.conn(requestURL);
+  }
+
+  conn(requestURL) {
+    this.request.open("get", requestURL); //請求伺服器連結，requestURL是上面宣告的網址
+    this.request.responseType = "json"; //得到的資料已JSON格式解讀
+    this.request.send(); //傳送資料
+    //載入資料到自己設定的地方，onload會等所有資料載入完自動執行
+    this.request.onload = () => {
+      this.locationInfos(this.request.response);
+    }
+  }
+
+  //整理各縣市的個別資料
+  locationInfos(localWeathers) {
+    let infoLs2 = localWeathers["records"].locations[0].location;
+
+    for (let i = 0; i < infoLs2.length; i++) {
+      let localW = infoLs2[i].weatherElement;
+
+      let data2 = {
+        location: infoLs2[i].locationName,
+        wx: this.getWeekWeather(localW[6]),
+        wxV: this.getWeekWeather(localW[6], 3),
+        t: this.getWeekWeather(localW[1]),
+        at: this.getWeekBodyT(localW[11], localW[5]),
+        uvi: this.getWeekWeather(localW[9], 2),
+        rh: this.getWeekWeather(localW[2]),
+        pop12h: this.getWeekWeather(localW[0]),
+        wd: this.getWeekWeather(localW[13]),
+      };
+
+      this.dataOK.push(data2);
+    }
+
+    this.getData(this.dataOK);
+  }
+
+  //計算一個禮拜的當前平均體感溫度
+  getWeekBodyT(minT, maxT) {
+    let infosOK = [];
+
+    for (let j = 0; j < minT.time.length; j++) {
+      if (j % 2 == 0) {
+        let min = parseInt(minT.time[j].elementValue[0].value);
+        let max = parseInt(maxT.time[j].elementValue[0].value);
+        infosOK.push(Math.round((min + max) / 2));
+      } else {
+        continue;
+      }
+    }
+    return infosOK;
+  }
+
+  //取出一個禮拜的當前資料
+  getWeekWeather(whatInfo, type = 1) {
+    let infosOK = [];
+
+    switch (type) {
+      case 1:
+        for (let j = 0; j < whatInfo.time.length; j++) {
+          if (j % 2 == 0) {
+            let thisInfo = whatInfo.time[j].elementValue[0].value;
+            infosOK.push(thisInfo);
+          } else {
+            continue;
+          }
+        }
+        break;
+
+      case 2:
+        for (let j = 0; j < whatInfo.time.length; j++) {
+          let thisInfo = whatInfo.time[j].elementValue[1].value;
+          infosOK.push(thisInfo);
+        }
+        break;
+
+      case 3:
+        for (let j = 0; j < whatInfo.time.length; j++) {
+          if (j % 2 == 0) {
+            let thisInfo = whatInfo.time[j].elementValue[1].value;
+            infosOK.push(thisInfo);
+          } else {
+            continue;
+          }
+        }
+        break;
+    }
+    return infosOK;
+  }
+}
+
+const monthEnglish = [
+  "JANUARY",
+  "FEBRUARY",
+  "MARCH",
+  "APRIL",
+  "MAY",
+  "JUNE",
+  "JULY",
+  "AUGUST",
+  "SEPTEMBER",
+  "OCTOber",
+  "NOVEMBER",
+  "DECEMBER",
+];
+
+const dayEnglish = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
+const dayChinese = ["日", "一", "二", "三", "四", "五", "六"];
